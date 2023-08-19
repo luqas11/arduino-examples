fc139e3abeeb7ac39d12472e28632bdf385ff3bf
luqas11 <lucascazas@hotmail.com>
2023-08-18 22:48:42 -0300

    Add IRReceiver sketch

---
+++ IRReceiver/IRReceiver.ino
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+#include <IRremote.hpp>
+
+const int IR_RECEIVER_PIN = 8;
+
+void setup()
+{
+  Serial.begin(115200);
+
+  // Start the receiver, and enable the built it led to give feedback when receiving signals
+  IrReceiver.begin(IR_RECEIVER_PIN, ENABLE_LED_FEEDBACK);
+}
+
+void loop()
+{
+  // Try to read any signal and decode it
+  if (IrReceiver.decode())
+  {
+    // If the protocol cannot be identified, print an error message
+    if (IrReceiver.decodedIRData.protocol == UNKNOWN)
+    {
+      Serial.println("Received noise or an unknown protocol");
+    }
+    // If the protocol can be identified, print the received command
+    else
+    {
+      Serial.println();
+      Serial.println("Received command:");
+      Serial.println(IrReceiver.decodedIRData.command);
+    }
+
+    // Enable the receiver to read the next signal
+    IrReceiver.resume();
+  }
+}
