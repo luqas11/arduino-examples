ba5227a8d8d881e11e9a02e09efe0007304fa7a1
luqas11 <lucascazas@hotmail.com>
2023-08-23 12:04:28 -0300

    Add CPURGBLed sketch

---
+++ CPURGBLed/CPULoadReader/CPULoadReader.cs
@@ -0,0 +1,87 @@
+﻿// To compile this .cs file into and .exe file, use an installation of Microsoft.NET.
+// In my case:
+// C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe CPULoadReader.cs
+using System;
+using System.Management;
+using System.IO.Ports;
+using System.Threading;
+
+namespace CPULoadReader {
+  class Program {
+    static SerialPort ArduinoPort;
+    // The baud rate of the Arduino's serial communication
+    const int BAUD_RATE = 9600;
+    // The delay between CPU load readings
+    const int DELAY_TIME_MS = 100;
+    // A char offset to prevent the terminal from printing non-printable characters (space is the first printable character of the ASCII table)
+    const char CHAR_OFFSET = ' ';
+
+    static void Main(String[] args) {
+      // Initialize the program and ask to input the COM port
+      Console.Title = "CPU Load Reader";
+      Console.WriteLine("----------------------");
+      Console.WriteLine("Starting CPU Load Reader");
+      Console.WriteLine("----------------------\n");
+      connectToCOM();
+    }
+
+    // Ask to input the COM port number and try to connect to the Arduino
+    static void connectToCOM() {
+      Console.WriteLine("Write the COM port number where the Arduino is connected and press ENTER:");
+      // Read user input
+      String comPortNumber = Console.ReadLine();
+
+      // Try to connect to Arduino through the given port
+      try {
+        ArduinoPort = new SerialPort();
+        ArduinoPort.PortName = "COM" + comPortNumber;
+        ArduinoPort.BaudRate = BAUD_RATE;
+        ArduinoPort.Open();
+      // If error, ask the user to retry
+      } catch {
+        Console.WriteLine("\n----------------------");
+        Console.WriteLine("Invalid COM port number, check the connection to the Arduino and try again.");
+        Console.WriteLine("----------------------\n");
+        connectToCOM();
+        return;
+      }
+
+      Console.WriteLine("\n----------------------");
+      Console.WriteLine("Connected");
+      Console.WriteLine("----------------------");
+
+      // When connection is successful, start sending CPU Load values
+      sendCPULoadValue();
+    }
+
+    // Send CPU load values to the Arduino indefinetly
+    static void sendCPULoadValue() {
+      ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_Processor");
+
+      // Try to get the current CPU load value and send it to the Arduino
+      try {
+        foreach(ManagementObject obj in searcher.Get()) {
+          // Get the load value as an int, between 0 and 100
+          int intValue = int.Parse(obj["LoadPercentage"].ToString());
+          // Divide the int by two and cast it to a char, to be able to send the value in a single byte
+          char charValue = (char) (intValue / 2);
+          // Add an offset to prevent the console to print non-printable characters
+          charValue += CHAR_OFFSET;
+          // Print the read values to the terminal
+          Console.WriteLine("\nCurrent value:");
+          Console.WriteLine("As int: " + intValue);
+          Console.WriteLine("As char: " + charValue);
+          // Send the char through the COM port
+          ArduinoPort.Write(charValue.ToString());
+        }
+      // If any error occurs, show an error message
+      } catch {
+        Console.WriteLine("Unable to read the current CPU load value.");
+      }
+
+      // Repeat all the process indefinetly, waiting 100ms between repetitions
+      Thread.Sleep(DELAY_TIME_MS);
+      sendCPULoadValue();
+    }
+  }
+}
---
+++ CPURGBLed/CPURGBLed.ino
@@ -0,0 +1,48 @@
+// The pins where the R, G and B components of the RGB LED are connected
+const int ledPins[] = {11, 10, 9};
+// The chat offset defined in the CPULoadReader for the outcoming characters
+const char CHAR_OFFSET = ' ';
+// The speed at which the color will change after receiving a value (inversely defined, e.g. a lower value will increase the speed)
+const int SPEED_FACTOR = 40;
+
+int currentValue = 1;
+float mean = 1;
+
+void setup()
+{
+  // Initialize serial communication and LED outputs
+  Serial.begin(9600);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+}
+
+void loop()
+{
+  // Read the serial port until a character is received
+  while (Serial.available() > 0)
+  {
+    char receivedChar = Serial.read();
+    // Apply a correction to the received character, based on the given offset
+    currentValue = (receivedChar - CHAR_OFFSET) * 2;
+  }
+
+  // Get a mean value between the received and the stored one, using the speed factor to weight them
+  mean = ((mean * (SPEED_FACTOR - 1) + currentValue) / SPEED_FACTOR);
+
+  // Print the current values
+  Serial.println();
+  Serial.println("Mean: " + (String)mean);
+  Serial.println("Current: " + (String)currentValue);
+
+  // Set the RGB outputs to the corresponding values
+  float valueR = map(mean, 0, 100, 0, 255);
+  analogWrite(ledPins[0], valueR);
+  float valueG = map(100 - mean, 0, 100, 0, 255);
+  analogWrite(ledPins[1], valueG);
+  float valueB = map(100 - mean, 0, 100, 0, 255);
+  analogWrite(ledPins[2], valueB);
+
+  // Wait some time to avoid excessive prints to the serial monitor
+  delay(50);
+}
